18e1873adb560ac0dea45fbcb142d15a2bcc4554
Reeman Singh <reemanl.singh422@gmail.com>
2023-06-08 23:47:45 +0800

    Moved navbar into its own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,9 @@
 import React from "react";
-
+import NavBar from "./components/NavBar";
 function App() {
   return (
     <>
-      <nav className="h-24 w-full fixed top-0 left-0 z-30 text-lg">
-        <div className="mx-auto max-w-full h-full p-4 flex items-center justify-between">
-          <a href="" className="px-4">
-            [[[[ LOGO ]]]]
-          </a>
-          <a href="" className="md:hidden">
-            =
-          </a>
-          <div className="md:flex space-x-1 hidden">
-            <a href="" className="px-4">
-              {" "}
-              About{" "}
-            </a>
-            <a href="" className="px-4">
-              {" "}
-              Projects{" "}
-            </a>
-            <a href="" className="px-4">
-              {" "}
-              Contact us{" "}
-            </a>
-          </div>
-        </div>
-      </nav>
-
+      <NavBar />
       <section className="mt-24">
         <p>Foo</p>
         <p>Foo</p>
