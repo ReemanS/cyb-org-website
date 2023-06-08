cf229e927c146abf466b6cd17cbcb99d0fb1057f
Reeman Singh <reemanl.singh422@gmail.com>
2023-06-08 23:22:40 +0800

    Added navbar skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,75 @@
 import React from "react";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <nav className=" h-24 w-full fixed text-lg">
+        <div className="mx-auto max-w-full h-full p-4 flex items-center justify-between">
+          <a href="" className="px-4">
+            [[[[LOGO]]]]
+          </a>
+          <div className="flex space-x-1">
+            <a href="" className="px-4">
+              {" "}
+              About{" "}
+            </a>
+            <a href="" className="px-4">
+              {" "}
+              Projects{" "}
+            </a>
+            <a href="" className="px-4">
+              {" "}
+              Contact us{" "}
+            </a>
+          </div>
+        </div>
+      </nav>
+      <section>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+        <p>Foo</p>
+      </section>
+    </>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         primary: "#21332f",
+      },
+      fontFamily: {
+        lato: ["Lato", "sans-serif"],
+        manrope: ["Manrope", "sans-serif"],
       },
     },
   },
