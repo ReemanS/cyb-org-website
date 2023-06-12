2d0f947c35be4d54d31ccf18c43c7a8bb0f809f4
Reeman Singh <reemanl.singh422@gmail.com>
2023-06-12 12:48:31 +0800

    Improved animations

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -27,12 +27,12 @@ function Nav() {
   // Variants for nav items
   const containerVariants = {
     hidden: { opacity: 0 },
-    show: { opacity: 1, transition: { delay: 0.1, staggerChildren: 0.2 } },
+    show: { opacity: 1, transition: { staggerChildren: 0.2 } },
   };
 
   const listItemVariants = {
     hidden: { opacity: 0, y: -30 },
-    show: { opacity: 1, y: 0 },
+    show: { opacity: 1, y: 0, transition: { duration: 0.2 } },
   };
 
   return (
